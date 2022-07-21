9edadb90a397637f4ddaeaa04fb284966b9efb2f
laludonisetiawan <laludonisetiawan@gmail.com>
2022-07-21 12:59:33 +0800

    add planing API - add body-parser

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const productRoutes = require('./src/routes/products');
 
-app.use('/', productRoutes);
+//middleware untuk menerima body request dari client 
+app.use(bodyParser.json()); // type json
+
+// tambahakn ketikak client mengkases api kita untuk menghiindari kesalahan cors origin pada borwser
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-ORIGIN', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET','POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next();
+})
+
+app.use('/v1/costumer', productRoutes);
 
 app.listen(4000);
--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,12 +1,15 @@
 // function untuk createproduct nantinya akan di panggil yang di buat dari controller 
 exports.createProduct = (req, res, next) => {
     //mengirimkan response data ketika client mengunjungi endpoint products
+    const name = req.body.name;
+    const description = req.body.description;
+    const price = req.body.price;
     res.json({
         message: 'create Product success',
         data: {
-            name: 'monitor',
-            description: 'new bnb 100% original',
-            price: 9000000,
+            name: name,
+            description: description,
+            price: price,
         }
     });
 
@@ -18,19 +21,22 @@ exports.createProduct = (req, res, next) => {
 
 
 
-exports.getAllProducts =  (req, res, next) => {
+exports.getAllProducts = (req, res, next) => {
+    const name = res.body.name;
+    const description = res.body.description;
+    const price = res.body.price;
     //mengirimkan response data ketika client mengunjungi endpoint products
     res.json({
         message: 'get All Products success!',
         data: [
             {
-                name: 'monitor',
-                description: 'new bnb 100% original',
-                price: 9000000, 
+                name: name,
+                description: description,
+                price: price,
             }
         ]
     });
 
     // agar proses eksekusi endpointnya berjalan ke endpoint selanjutkan tambahkan parameter next();
     next();
- }
+}
